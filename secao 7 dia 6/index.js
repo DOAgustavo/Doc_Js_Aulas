bf53aaf73c5cd0f3367101d8a71627b89203474a
Giovanni Gustavo Santos Sousa <giovanni.sousa@ceramfix.local>
2025-10-13 15:37:28 -0300

    finalizando o dia

--- secao 7 dia 6/index.js
+++ secao 7 dia 6/index.js
@@ -8,6 +8,22 @@ const colors=[
 const btn = document.getElementById('btn')
 
 btn.addEventListener('click', function(){
-    document.body.style.background = colors[1]
+
+    const color =document.querySelector('.color')
+    const randomCor = radomColor()
+    document.body.style.background = colors[randomCor]
+    color.textContent = colors[randomCor]
+
+    //console.log(randomCor)
+    //console.log(randomCor)
+   // console.log('verificando')
 })
-console.log('verificando')
+
+
+function radomColor (){
+    return Math.floor(Math.random()*colors.length)
+}
+
+/*O que é querySelector?
+
+É um método do DOM (Document Object Model) que retorna o primeiro elemento que bate com um seletor CSS que você passar. */
