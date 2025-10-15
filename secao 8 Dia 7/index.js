5c086b623d8ca0b273310f027800d391f0f35a5c
Giovanni Gustavo Santos Sousa <giovanni.sousa@ceramfix.local>
2025-10-15 15:30:04 -0300

    adicionando os ifs para aproximação

--- secao 8 Dia 7/index.js
+++ secao 8 Dia 7/index.js
@@ -1,7 +1,23 @@
 let computerNumber  
-
+let userNumbers=[]
 function init(){
 
     computerNumber = Math.floor(Math.random() * 100 + 1)
     console.log(computerNumber)
+}
+
+function compareNumbers(){
+    const userNumber = Number(document.getElementById('inputBox').value)
+    userNumbers.push(" "+userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
+
+    if(userNumber > computerNumber){
+        document.getElementById('textOutput').innerHTML = 'Your number is too high'
+        document.getElementById('inputBox').value=''
+    }else if( userNumber< computerNumber){
+        document.getElementById('textOutput').innerHTML = 'Your number is too low'
+        document.getElementById('inputBox').value=''
+    }else{
+        document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
     }
+}
